3ea208235329a9ba4e170827af769b732c3047f8
Wrench1666 <mrtehnik330@gmail.com>
2025-09-18 20:13:58 +0300

    -

--- thr/thr1.cpp
+++ thr/thr1.cpp
@@ -11,41 +11,190 @@ std::mutex counter_mutex;
 int val;
 #define REPEATER 100
 
-void f1(int a)
+void add1or2T(int a)
+{
+	for (int i = 0;i < REPEATER;i++)
+	{
+		for (int j = 0;j < 1000001;j++)
+		{
+			counter+=a;
+		}
+	}
+}
+
+void sub1or2T(int a)
 	{
 		for (int i = 0;i < REPEATER;i++)
 		{
 			for (int j = 0;j < 1000001;j++)
 			{
-				//действие +
+				counter-=a;
 			}
 		}
 	}
 
-void f2(int a)
-	{
-		for (int i = 0;i < REPEATER;i++)
-		{
-			for (int j = 0;j < 1000001;j++)
-			{
-				//действие -
-			}
-		}
-	}
-
-void test1(void)
+void test1T(int a)
 {
 	clock_t time0, time1;
 	counter = 0;
 	time0 = clock();
-	//тест
-	time1 = clock();
-	double period = ((time1 - time0) * 10000) / CLOCKS_PER_SEC;
+	add1or2T(a);
+	sub1or2T(a);
+	double period = ((clock() - time0) * 10000) / CLOCKS_PER_SEC;
+	std::cout << "Time= " << period;
+}
+
+void test2T(int a)
+{
+	clock_t time0, time1;
+	counter = 0;
+	time0 = clock();
+	std::thread th1(add1or2T, a);
+	std::thread th2(sub1or2T, a);
+	th1.join();
+	th2.join();
+	double period = ((clock() - time0) * 10000) / CLOCKS_PER_SEC;
+	std::cout << "Time= " << period;
+}
+
+void addmutex(int a)
+{
+	for (int i = 0; i < REPEATER; i++)
+	{
+		for (int j = 0; j < 1000001; j++)
+		{
+			counter_mutex.lock();
+			counter += a;
+			counter_mutex.unlock();
+		}
+	}
+}
+
+void submutex(int a)
+{
+	for (int i = 0; i < REPEATER; i++)
+	{
+		for (int j = 0; j < 1000001; j++)
+		{
+			counter_mutex.lock();
+			counter -= a;
+			counter_mutex.unlock();
+		}
+	}
+}
+
+void testmutex(int a)
+{
+	clock_t time0, time1;
+	counter = 0;
+	time0 = clock();
+	std::thread th1(addmutex, a);
+	std::thread th2(submutex, a);
+	th1.join();
+	th2.join();
+	double period = ((clock() - time0) * 10000) / CLOCKS_PER_SEC;
+	std::cout << "Time= " << period;
+}
+
+void addatomic(int a)
+{
+	for (int i = 0; i < REPEATER; i++)
+	{
+		for (int j = 0; j < 1000001; j++)
+		{
+			counter2.fetch_add(a);
+		}
+	}
+}
+
+void subatomic(int a)
+{
+	for (int i = 0; i < REPEATER; i++)
+	{
+		for (int j = 0; j < 1000001; j++)
+		{
+			counter2.fetch_sub(a);
+		}
+	}
+}
+
+void testatomic(int a)
+{
+	clock_t time0, time1;
+	counter = 0;
+	time0 = clock();
+	std::thread th1(addatomic, a);
+	std::thread th2(subatomic, a);
+	th1.join();
+	th2.join();
+	double period = ((clock() - time0) * 10000) / CLOCKS_PER_SEC;
+	std::cout << "Time= " << period;
+}
+
+void addcs(int a)
+{
+	for (int i = 0; i < REPEATER; i++)
+	{
+		for (int j = 0; j < 1000001; j++)
+		{
+			EnterCriticalSection(&win_cs);
+			counter += a;
+			LeaveCriticalSection(&win_cs);
+		}
+	}
+}
+
+void subcs(int a)
+{
+	for (int i = 0; i < REPEATER; i++)
+	{
+		for (int j = 0; j < 1000001; j++)
+		{
+			EnterCriticalSection(&win_cs);
+			counter -= a;
+			LeaveCriticalSection(&win_cs);
+		}
+	}
+}
+
+void testcs(int a)
+{
+	clock_t time0, time1;
+	counter = 0;
+	time0 = clock();
+	std::thread th1(addcs, a);
+	std::thread th2(subcs, a);
+	th1.join();
+	th2.join();
+	double period = ((clock() - time0) * 10000) / CLOCKS_PER_SEC;
 	std::cout << "Time= " << period;
 }
 
 int main()
 {
 	std::cout << "\nEnter a value: ";
-	//std::cin >> b;
+	std::cin >> val;
+
+	test1T(val);
+	std::cout << "\nCounter = " << counter;
+	counter = 0;
+	std::cout << "\n\n2T\n";
+
+	test2T(val);
+	std::cout << "\nCounter = " << counter;
+	counter = 0;
+	std::cout << "\n\nMutex\n";
+
+	testmutex(val);
+	std::cout << "\nCounter = " << counter;
+	counter = 0;
+	std::cout << "\n\nAtomic\n";
+
+	testatomic(val);
+	std::cout << "\nCounter = " << counter2;
+	std::cout << "\n\nCritical Section\n";
+
+	InitializeCriticalSection(&win_cs);
+	testcs(val);
+	std::cout << "\nCounter = " << counter;
 }
